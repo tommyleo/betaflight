c0ec7d4ebddc7c1ae6ecbdea5d3540d8de0b6468
tommyleo <tleognani@gmail.com>
2016-08-10 20:39:48 +0200

    New Targer Sparky2

---
+++ src/main/target/SPARKY2/target.mk
@@ -0,0 +1,8 @@
+F405_TARGETS   += $(TARGET)
+FEATURES       += VCP ONBOARDFLASH
+
+TARGET_SRC = \
+            drivers/accgyro_spi_mpu6000.c \
+            drivers/accgyro_spi_mpu6500.c \
+            drivers/accgyro_mpu6500.c
+            
